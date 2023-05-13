61314b472201db62c116f4a80772782539e37303
Aaron Walsman <aaronwalsman@gmail.com>
2023-05-13 16:41:40 -0700

    more mild cleanup

--- plot_vizdoom.sh
+++
@@ -1,2 +0,0 @@
-python scripts/plot_results.py --inputs "../impossibly-good-eval/evaluation_compiled/compiled_vizdoom/ImpossiblyGood-MonsterRoom-v0_*/" --outputs ./visualization/monster_room;
-python scripts/plot_results.py --inputs "../impossibly-good-eval/evaluation_compiled/compiled_vizdoom/ImpossiblyGood-MonsterHall-v0_*/" --outputs ./visualization/monster_hall;
